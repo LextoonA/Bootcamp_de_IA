2b67cb069cf9a7ed03b3416cc7acd91172c3974f
LextoonA <lexcdlm@gmail.com>
2024-09-10 20:39:34 -0500

    Variables Python actualizadas

--- variables.py
+++ variables.py
@@ -6,3 +6,20 @@ nombre ="Alex"
 nombre ="Alexander"
 #Las variables son modificables
 print (nombre)
+#Slide 204-218
+numero = 10
+numero += 5
+numero -= 5
+print (numero)
+#Concatenar
+bienvenida = "Hola " + nombre + " feliz noche"
+print (bienvenida)
+#Concatenar con f strings
+Bienvenido= f"Hola {nombre} ¿Cómo estás?"
+print (Bienvenido)
+#in not in 
+print ("Alexander" in bienvenida)
+print ("Alexander" not in bienvenida)
+#Snake Case
+nombreCompletoDeTuProfe = "Jheyson Galvis" #Para JavaScript
+nombre_completo_de_tu_profe = "Jheyson Galvis" #Para phyton
