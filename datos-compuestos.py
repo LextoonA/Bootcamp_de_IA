85115bc41171b7c702b22347ec9a8c87c92dc8c7
andres naranjo <santo2828@gmail.com>
2024-09-21 10:16:39 -0500

    datos-compuestos

---
+++ datos-compuestos.py
@@ -0,0 +1,20 @@
+#datos compuestos de python
+#listas
+listas = [1,2,3,4,5,6,7,8,9,10]
+
+#lista dinamica
+listas = ["alex", 1.65, True, "andres", False, 2, 3, 4, 5, 6]
+listas[0] = "Santiago"
+print(listas[0])
+
+#tuplas
+tuplas = ("huevos pericos", 8.45, True, "chocolate", 3)
+print(tuplas[0])
+
+#diccionario
+diccionario = {
+    "nombre": "Alex",
+    "apellido": "Granja",
+    "estatura": 1.75
+}
+print(diccionario["nombre"])
---
+++ main.py
@@ -0,0 +1,26 @@
+name_estudiante = "Andre Santiago"
+last_name_estudiante = "Arias"
+print(name_estudiante[0])
+print(name_estudiante[1])
+print(name_estudiante[2])
+print(name_estudiante[-2])
+
+print(5*name_estudiante)
+print(name_estudiante.lower())
+
+#mayuscualas
+print(name_estudiante.upper())
+
+print(name_estudiante.strip())
+
+x= 10
+y=5.6
+z=1.2E6
+
+is_true = True
+is_false = False
+print(is_false)
+print(is_true)
+print(type(x))
+print(type(y))
+print(z)
